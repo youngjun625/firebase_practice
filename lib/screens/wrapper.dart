b82963060e9d6213e3b474cd6e582f2efde9712c
Young Jun Kim <youngjun625@korea.ac.kr>
2022-04-03 22:49:40 +0900

    L3

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,17 @@
+import 'package:firebase/screens/authenticate/authenticate.dart';
+import 'package:firebase/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    
+
+  
+//return either Home or Authenticate widget
+    return Authenticate();
+  
+  }
+}
